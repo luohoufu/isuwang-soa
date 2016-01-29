50ddd1d2da4eff02f41e7ed34b6213dc1b5fcc9c
craneding <crane.ding@163.com>
2016-01-29 14:39:37 +0800

    加入soa.base的设置

--- isuwang-soa-maven-plugin/src/main/java/com/isuwang/soa/maven/plugin/RunContainerPlugin.java
+++ isuwang-soa-maven-plugin/src/main/java/com/isuwang/soa/maven/plugin/RunContainerPlugin.java
@@ -10,6 +10,7 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 
+import java.io.File;
 import java.net.URL;
 import java.net.URLClassLoader;
 
@@ -28,24 +29,24 @@ public class RunContainerPlugin extends SoaAbstractMojo {
 
         getLog().info("bundle:" + project.getGroupId() + ":" + project.getArtifactId() + ":" + project.getVersion());
 
+        System.setProperty("soa.base", new File(project.getBuild().getOutputDirectory()).getAbsolutePath().replace("/target/classes", ""));
+
         final String mainClass = Engine.class.getName();
 
         IsolatedThreadGroup threadGroup = new IsolatedThreadGroup(mainClass);
-        Thread bootstrapThread = new Thread(threadGroup, new Runnable() {
-            public void run() {
-                try {
-                    //ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        Thread bootstrapThread = new Thread(threadGroup, () -> {
+            try {
+                //ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
 
-                    URL[] urls = ((URLClassLoader) Thread.currentThread().getContextClassLoader()).getURLs();
+                URL[] urls = ((URLClassLoader) Thread.currentThread().getContextClassLoader()).getURLs();
 
-                    ClassLoaderManager.shareClassLoader = new ShareClassLoader(urls);
-                    ClassLoaderManager.platformClassLoader = new PlatformClassLoader(urls);
-                    ClassLoaderManager.appClassLoaders.add(new AppClassLoader(urls));
+                ClassLoaderManager.shareClassLoader = new ShareClassLoader(urls);
+                ClassLoaderManager.platformClassLoader = new PlatformClassLoader(urls);
+                ClassLoaderManager.appClassLoaders.add(new AppClassLoader(urls));
 
-                    Engine.main(new String[]{});
-                } catch (Exception e) {
-                    Thread.currentThread().getThreadGroup().uncaughtException(Thread.currentThread(), e);
-                }
+                Engine.main(new String[]{});
+            } catch (Exception e) {
+                Thread.currentThread().getThreadGroup().uncaughtException(Thread.currentThread(), e);
             }
         }, mainClass + ".main()");
         bootstrapThread.setContextClassLoader(getClassLoader());
