81cd4e3035a0656046269bf849cff55400bdd424
tangliu <tangliu@iplas.com.cn>
2016-02-03 11:14:30 +0800

    服务端使用xml配置filter

--- isuwang-soa-container/src/main/java/com/isuwang/soa/container/registry/RegistryContainer.java
+++ isuwang-soa-container/src/main/java/com/isuwang/soa/container/registry/RegistryContainer.java
@@ -1,16 +1,22 @@
 package com.isuwang.soa.container.registry;
 
 import com.isuwang.soa.container.Container;
+import com.isuwang.soa.container.Main;
 import com.isuwang.soa.container.spring.SpringContainer;
 import com.isuwang.soa.core.Service;
 import com.isuwang.soa.core.SoaBaseProcessor;
+import com.isuwang.soa.core.filter.Filter;
 import com.isuwang.soa.core.filter.container.ContainerFilterChain;
 import com.isuwang.soa.core.filter.container.ProviderTimesFilter;
 import com.isuwang.soa.core.filter.container.SlowTimeServiceFilter;
+import com.isuwang.soa.core.filter.container.xml.SoaFilter;
+import com.isuwang.soa.core.filter.container.xml.SoaFilters;
 import com.isuwang.soa.registry.RegistryAgent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.xml.bind.JAXB;
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Method;
@@ -30,8 +36,23 @@ public class RegistryContainer implements Container {
 
     @Override
     public void start() {
-        ContainerFilterChain.addFilter(new ProviderTimesFilter());
-        ContainerFilterChain.addFilter(new SlowTimeServiceFilter());
+
+        try {
+            InputStream is = new BufferedInputStream(Main.loadInputStreamInClassLoader("filters-server.xml"));
+
+            SoaFilters soaFilters = JAXB.unmarshal(is, SoaFilters.class);
+            for (SoaFilter soaFilter : soaFilters.getSoaFilter()) {
+
+                Class filterClass = RegistryContainer.class.getClassLoader().loadClass(soaFilter.getRef());
+                Filter filter = (Filter) filterClass.newInstance();
+
+                ContainerFilterChain.addFilter(filter);
+                LOGGER.info("service load filter {} with path {}", soaFilter.getName(), soaFilter.getRef());
+            }
+
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        }
 
         RegistryAgent.getInstance().start();
         RegistryAgent.getInstance().setProcessorMap(processorMap);
