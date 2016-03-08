70d3af877ced34b814e1b2f0767427b315c300c2
tangliu <tangliu@iplas.com.cn>
2016-03-08 16:12:16 +0800

    QPSstatFilter

--- isuwang-soa-container/src/main/java/com/isuwang/soa/container/filter/QPSStatFilter.java
+++ isuwang-soa-container/src/main/java/com/isuwang/soa/container/filter/QPSStatFilter.java
@@ -1,7 +1,11 @@
 package com.isuwang.soa.container.filter;
 
 import com.isuwang.soa.core.filter.FilterChain;
+import com.isuwang.soa.monitor.api.MonitorServiceClient;
+import com.isuwang.soa.monitor.api.domain.QPSStat;
 import org.apache.thrift.TException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Calendar;
 import java.util.Timer;
@@ -15,12 +19,19 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @date 16/3/7
  */
 public class QPSStatFilter implements StatusFilter {
-    private final int period = 5;
+
+    private final long period = 5 * 60 * 1000L;
     private final AtomicInteger callCount = new AtomicInteger(0);
     private final Timer timer = new Timer("QPSStatFilter-Timer");
+    private static final Logger LOGGER = LoggerFactory.getLogger(QPSStatFilter.class);
+
+    private MonitorServiceClient monitorServiceClient;
 
     @Override
     public void init() {
+
+        monitorServiceClient = new MonitorServiceClient();
+
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
@@ -28,7 +39,17 @@ public class QPSStatFilter implements StatusFilter {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                //TODO upload data and set zero into AtomicInteger
+
+                try {
+                    QPSStat qpsStat = new QPSStat();
+                    qpsStat.setPeriod((int) period / 1000 / 60);
+                    qpsStat.setCallCount(callCount.get());
+                    monitorServiceClient.uploadQPSStat(new QPSStat());
+                } catch (Exception e) {
+                    LOGGER.error(e.getMessage());
+                } finally {
+                    callCount.set(0);
+                }
             }
         }, calendar.getTime(), period);
     }
