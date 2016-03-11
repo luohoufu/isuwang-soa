36d2a90d606ce7d36dc504725c6a61296bc24aad
craneding <crane.ding@163.com>
2016-03-11 10:36:20 +0800

    延迟qps第一次上送时间

--- isuwang-soa-container/src/main/java/com/isuwang/soa/container/filter/QPSStatFilter.java
+++ isuwang-soa-container/src/main/java/com/isuwang/soa/container/filter/QPSStatFilter.java
@@ -32,7 +32,7 @@ public class QPSStatFilter implements StatusFilter {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
-        calendar.add(Calendar.MILLISECOND, (int) period);
+        calendar.add(Calendar.MILLISECOND, (int) period * 3);
 
         timer.schedule(new TimerTask() {
             @Override
