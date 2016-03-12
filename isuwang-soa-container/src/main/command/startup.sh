7b17b16d1f36b7cecdde0ceb1a409ed040124dfb
craneding <crane.ding@163.com>
2016-03-12 18:24:36 +0800

    加入发布模式不启用控制台日志

--- isuwang-soa-container/src/main/command/startup.sh
+++ isuwang-soa-container/src/main/command/startup.sh
@@ -24,6 +24,8 @@ fi
 jvm_opts="-Xms256m -Xmx256m -Xloggc:$logdir/gc.log -XX:+PrintGCDateStamps -XX:+PrintGCDetails -XX:+PrintGC"
 debug_opts="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5000"
 soa_base="-Dsoa.base=$workdir/../ -Dsoa.run.mode=native"
+#user_opts="-Dsoa.container.port=9090 -Dsoa.zookeeper.host=127.0.0.1:2181 -Dio.netty.leakDetectionLevel=advanced"
+user_opts=""
 
-nohup java $jvm_opts $soa_base $debug_opts -cp ./isuwang-soa-bootstrap.jar com.isuwang.soa.bootstrap.Bootstrap >> $logdir/catalina.out 2>&1 &
+nohup java $jvm_opts $soa_base $debug_opts $user_opts -cp ./isuwang-soa-bootstrap.jar com.isuwang.soa.bootstrap.Bootstrap >> $logdir/catalina.out 2>&1 &
 echo $! > $logdir/pid.txt
