7ac629b30f57f5b9ad1f0a9e0052c041b4dd8387
craneding <crane.ding@163.com>
2016-03-13 21:45:16 +0800

    加入HeapDumpOnOutOfMemoryError

--- isuwang-soa-container/src/main/command/startup.sh
+++ isuwang-soa-container/src/main/command/startup.sh
@@ -21,10 +21,10 @@ if [ ! -d "$logdir" ]; then
 	mkdir "$logdir"
 fi
 
-jvm_opts="-Xms256m -Xmx256m -Xloggc:$logdir/gc.log -XX:+PrintGCDateStamps -XX:+PrintGCDetails -XX:+PrintGC"
+jvm_opts="-Xms256m -Xmx256m -Xloggc:$logdir/gc.log -XX:+PrintGCDateStamps -XX:+PrintGCDetails -XX:+PrintGC -XX:+HeapDumpOnOutOfMemoryError"
 debug_opts="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5000"
 soa_base="-Dsoa.base=$workdir/../ -Dsoa.run.mode=native"
-#user_opts="-Dsoa.container.port=9090 -Dsoa.zookeeper.host=127.0.0.1:2181 -Dio.netty.leakDetectionLevel=advanced"
+#user_opts="-Dsoa.container.port=9090 -Dsoa.zookeeper.host=127.0.0.1:2181 -Dio.netty.leakDetectionLevel=advanced -XX:MaxDirectMemorySize=128M"
 user_opts=""
 
 nohup java $jvm_opts $soa_base $debug_opts $user_opts -cp ./isuwang-soa-bootstrap.jar com.isuwang.soa.bootstrap.Bootstrap >> $logdir/catalina.out 2>&1 &
