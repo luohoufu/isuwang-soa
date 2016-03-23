f0848c7855f6b75cc9dcdf9fa890b3b77d1cfdb3
craneding <crane.ding@163.com>
2016-03-23 10:18:10 +0800

    更新启动脚本监控的默认选项

--- isuwang-soa-container/src/main/command/startup.sh
+++ isuwang-soa-container/src/main/command/startup.sh
@@ -37,7 +37,7 @@ fi
 # env option(priority than vm option)
 # soa_container_port (default: 9090)
 # soa_zookeeper_host (default: 127.0.0.1:2181)
-# soa_monitor_enable (default: true)
+# soa_monitor_enable (default: false)
 # soa_container_usethreadpool (default: true)
 # soa_core_pool_size (default: Runtime.getRuntime().availableProcessors() * 2)
 # soa_remoting_mode (default: remote [remote/local])
@@ -47,7 +47,7 @@ fi
 # vm option
 # soa.container.port (default: 9090)
 # soa.zookeeper.host (default: 127.0.0.1:2181)
-# soa.monitor.enable (default: true)
+# soa.monitor.enable (default: false)
 # soa.container.usethreadpool (default: true)
 # soa.core.pool.size (default: Runtime.getRuntime().availableProcessors() * 2)
 # soa.remoting.mode (default: remote [remote/local])
@@ -67,7 +67,7 @@ fi
 
 JVM_OPTS="-Xms256m -Xmx256m -Xloggc:$logdir/gc.log -XX:+PrintGCDateStamps -XX:+PrintGCDetails -XX:+PrintGC -XX:+HeapDumpOnOutOfMemoryError"
 DEBUG_OPTS=""
-SOA_BASE="-Dsoa.base=$workdir/../ -Dsoa.run.mode=native"
+SOA_BASE="-Dsoa.base=$workdir/../ -Dsoa.run.mode=native -Dsoa.monitor.enable=true"
 USER_OPTS=""
 
 nohup java $JVM_OPTS $SOA_BASE $DEBUG_OPTS $USER_OPTS -cp ./isuwang-soa-bootstrap.jar com.isuwang.soa.bootstrap.Bootstrap >> $logdir/catalina.out 2>&1 &
