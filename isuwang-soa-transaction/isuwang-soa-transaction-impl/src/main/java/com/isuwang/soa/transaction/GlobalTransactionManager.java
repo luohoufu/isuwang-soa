749bb975a76ccac487bc5851fac1cabc9f79c7f3
craneding <crane.ding@163.com>
2016-04-12 19:41:34 +0800

    add transaction scheduler

--- isuwang-soa-transaction/isuwang-soa-transaction-impl/src/main/java/com/isuwang/soa/transaction/GlobalTransactionManager.java
+++ isuwang-soa-transaction/isuwang-soa-transaction-impl/src/main/java/com/isuwang/soa/transaction/GlobalTransactionManager.java
@@ -2,6 +2,8 @@ package com.isuwang.soa.transaction;
 
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /**
  * Global Transaction Manager
  *
@@ -10,16 +12,19 @@ import org.springframework.transaction.annotation.Transactional;
  */
 public class GlobalTransactionManager {
 
-    public void init() {
-        System.out.println("init");
-    }
-
-    public void destory() {
-        System.out.println("destory");
-    }
+    private AtomicBoolean working = new AtomicBoolean(false);
 
     @Transactional(value = "globalTransaction", rollbackFor = Exception.class)
     public void doJob() {
+        if(working.get())
+            return;
 
+        working.set(true);
+
+        try {
+            System.out.println("--- done ---");
+        } finally {
+            working.set(false);
+        }
     }
 }
