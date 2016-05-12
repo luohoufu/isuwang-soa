b26545de059b36a9d9ff7955dea165a6c6d78aae
唐流 <tangliu@iplas.com.cn>
2016-05-12 11:05:05 +0800

    去掉类型

--- isuwang-scala-dbc/src/main/scala/com/isuwang/scala/dbc/Action.scala
+++ isuwang-scala-dbc/src/main/scala/com/isuwang/scala/dbc/Action.scala
@@ -3,10 +3,10 @@ package com.isuwang.scala.dbc
 import org.slf4j.{Logger, LoggerFactory}
 
 /**
- * Design by contract Handler
- * @author craneding
- * @date 15/12/4
- */
+  * Design by contract Handler
+  * @author craneding
+  * @date 15/12/4
+  */
 abstract class Action[Output] {
   def execute: Output = {
     try {
@@ -31,40 +31,40 @@ abstract class Action[Output] {
   }
 
   /**
-   * 输入检查：查询、新增、更新、删除等输入条件
-   */
+    * 输入检查：查询、新增、更新、删除等输入条件
+    */
   def inputCheck
 
   /**
-   * 前置条件检查：动作、状态等业务逻辑
-   */
+    * 前置条件检查：动作、状态等业务逻辑
+    */
   def preCheck
 
   /**
-   * 动作
-   */
+    * 动作
+    */
   def action: Output
 
   /**
-   * 后置条件检查
-   */
+    * 后置条件检查
+    */
   def postCheck
 }
 
 /**
- * Design by contract Entity Handler
- *
- * 一般的，实现类构造子为 OrderCancelAction(order, input1, input2)
- * @author craneding
- * @date 15/12/4
- */
-abstract class EntityAction[Entity <: java.io.Serializable, Output] extends Action[Output] {
+  * Design by contract Entity Handler
+  *
+  * 一般的，实现类构造子为 OrderCancelAction(order, input1, input2)
+  * @author craneding
+  * @date 15/12/4
+  */
+abstract class EntityAction[Entity, Output] extends Action[Output] {
 
   val entity: Entity
 
   /**
-   * 不变量检查
-   */
+    * 不变量检查
+    */
   def invariantCheck
 
   override def execute: Output = {
