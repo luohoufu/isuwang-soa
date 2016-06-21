d3aebb50205fe8b074227bffda20b917e391b0c8
tangliu <tangliu@mail2.sysu.edu.cn>
2016-06-21 19:39:52 +0800

    ipPattern初步

--- dapeng-route/dapeng-route-impl/src/main/java/com/isuwang/dapeng/route/parse/RouteParser.java
+++ dapeng-route/dapeng-route-impl/src/main/java/com/isuwang/dapeng/route/parse/RouteParser.java
@@ -1,8 +1,116 @@
 package com.isuwang.dapeng.route.parse;
 
+import com.isuwang.dapeng.route.MatcherId;
+import com.isuwang.dapeng.route.Matchers;
+import com.isuwang.dapeng.route.Route;
+import com.isuwang.dapeng.route.pattern.IpPattern;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by tangliu on 2016/6/19.
  */
 public class RouteParser {
+
+
+    private static Pattern routePattern = Pattern.compile("^(\\S+)\\s+match\\s+([~]?)([^\"]+)\"(\\S+)\"\\s+=>\\s+([~]?)ip\"([^\"]+)\"");
+
+    /**
+     * 将配置文件，解析成路由规则列表
+     *
+     * @param source
+     * @return
+     */
+    public static List<Route> parseAll(String source) throws IOException {
+
+        List<Route> routes = new ArrayList<>();
+
+        source = "operatorId match ~%\"1024n+0..9\" => ip\"1.2.3/24\"\n" +
+                "      operatorId match ~%\"1024n+0..9\" => ~ip\"1.2.3.4\"\n" +
+                "      operatorId match ~n\"10\" => ip\"1.2.3.4\"\n" +
+                "      operatorId match ~n\"10..20\n => ip\"1.2.3.4\"\n" +
+                "      methodName match r\"get.*\" => ip\"1.2.3.4\"\n" +
+                "      callerFrom match s\"app\" => ip\"1.2.3.4\"\n" +
+                "      ip match ip\"1.2.3/24\" => ip\"1.2.3.4\"\n" +
+                "      otherwise => ip\"1.2.3.4\"";
+
+
+        BufferedReader br = new BufferedReader(new StringReader(source));
+
+        String line = "";
+        while ((line = br.readLine()) != null) {
+
+            line = line.trim();
+
+            Matcher matcher = routePattern.matcher(line);
+            if (matcher.find()) {
+                if (matcher.group(2).equals("~")) {
+
+                } else {
+                    switch (matcher.group(3)) {
+                        case "r":
+                            break;
+                        case "kv":
+                            break;
+                        case "ip":
+                            routes.add(generateIpRoute(matcher.group(4)));
+                            break;
+                        case "n":
+                            break;
+                        case "s":
+                            break;
+                        case "%":
+                            break;
+                        default:
+                            break;
+                    }
+
+                }
+            }
+
+        }
+        return routes;
+    }
+
+
+    /**
+     * 生成作用于ip的规则
+     *
+     * @return
+     */
+    private static Route generateIpRoute(String str) {
+
+        Route route = new Route();
+        Matchers matchers = new Matchers();
+        com.isuwang.dapeng.route.Matcher matcher = new com.isuwang.dapeng.route.Matcher();
+
+        matcher.setMatcherID(MatcherId.IP);
+
+        com.isuwang.dapeng.route.pattern.Pattern pattern = IpPattern.parse(str);
+        List<com.isuwang.dapeng.route.pattern.Pattern> patterns = new ArrayList<>();
+        patterns.add(pattern);
+        matcher.setPatterns(patterns);
+
+        List<com.isuwang.dapeng.route.Matcher> matcherList = new ArrayList<>();
+        matcherList.add(matcher);
+        matchers.setMatchers(matcherList);
+        route.setLeft(matchers);
+
+        return route;
+    }
+
+    public static void main(String[] args) {
+        try {
+            parseAll("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- dapeng-route/dapeng-route-impl/src/main/java/com/isuwang/dapeng/route/pattern/IpPattern.java
+++ dapeng-route/dapeng-route-impl/src/main/java/com/isuwang/dapeng/route/pattern/IpPattern.java
@@ -1,5 +1,10 @@
 package com.isuwang.dapeng.route.pattern;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.regex.Matcher;
+
 /**
  * Created by tangliu on 2016/6/19.
  */
@@ -8,6 +13,15 @@ public class IpPattern extends Pattern {
     public String ip;
 
     public int mask;
+
+    public IpPattern() {
+    }
+
+    public IpPattern(String ip, int mask) {
+
+        this.ip = ip;
+        this.mask = mask;
+    }
 
     public String getIp() {
         return ip;
@@ -23,5 +37,57 @@ public class IpPattern extends Pattern {
 
     public void setMask(int mask) {
         this.mask = mask;
+    }
+
+    public static java.util.regex.Pattern IP_PATTERN = java.util.regex.Pattern.compile("([\\d|.]+)([/]([\\d]+))?");
+
+    /**
+     * 将类似于"1.2.3.4/32"的字符串转为IpPattern
+     *
+     * @param str
+     * @return
+     */
+    public static IpPattern parse(String str) {
+
+        str = str.trim();
+        Matcher matcher = IP_PATTERN.matcher(str);
+        if (!matcher.find())
+            return null;
+
+        String ip = matcher.group(1);
+        for (int i = 0; i < (4 - matcher.group(1).split("[.]").length); i++) {
+            ip += ".0";
+        }
+
+        int mask = matcher.group(3) == null ? 32 : Integer.valueOf(matcher.group(3));
+
+        return new IpPattern(ip, mask);
+    }
+
+    public String toString() {
+        return "ip: " + this.ip + "  mask:" + this.mask;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        String source = "1.2.3.4\n" +
+                "1.2.3.4/32\n" +
+                "1.2.3/24\n" +
+                "1.2.5/16\n" +
+                "1.2/16\n" +
+                "1.5/8\n" +
+                "1/8\n" +
+                "1.2.3.123/25";
+
+
+        BufferedReader br = new BufferedReader(new StringReader(source));
+
+        String line = "";
+        while ((line = br.readLine()) != null) {
+
+            IpPattern ip = IpPattern.parse(line);
+            if (ip != null)
+                System.out.println(ip.toString());
+        }
     }
 }
