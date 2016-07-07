02d1c9f5b4e554b5b8a2469f99924c1c5b5ec794
tangliu <tangliu@mail2.sysu.edu.cn>
2016-07-07 19:39:19 +0800

    还原先，前面的做法会导致日志出不来

--- dapeng-bootstrap/src/main/java/com/isuwang/dapeng/bootstrap/classloader/AppClassLoader.java
+++ dapeng-bootstrap/src/main/java/com/isuwang/dapeng/bootstrap/classloader/AppClassLoader.java
@@ -18,14 +18,9 @@ public class AppClassLoader extends URLClassLoader {
     @Override
     protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
 
-        try {
+        if (name.startsWith("com.isuwang.dapeng.core") || name.startsWith("com.isuwang.org.apache.thrift") || name.startsWith("com.isuwang.dapeng.transaction.api")
+                || name.startsWith("com.google.gson"))
             return ClassLoaderManager.shareClassLoader.loadClass(name);
-        } catch (ClassNotFoundException e) {
-            return super.loadClass(name, resolve);
-        }
-//        if(name.startsWith("com.isuwang.dapeng.core") || name.startsWith("com.isuwang.org.apache.thrift") || name.startsWith("com.isuwang.dapeng.transaction.api")
-//                || name.startsWith("com.google.gson"))
-//            return ClassLoaderManager.shareClassLoader.loadClass(name);
-//        return super.loadClass(name, resolve);
+        return super.loadClass(name, resolve);
     }
 }
